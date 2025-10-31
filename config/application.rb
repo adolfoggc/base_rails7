43bd62d1a5eaa6ea5fc16ac716f1fc468305231d
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-10-31 11:27:26 -0300

    Add I18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module BaseProject7
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.i18n.default_locale = "pt-br".to_sym
   end
 end
